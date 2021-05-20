349a2e7008e73815e3cc34b12b560d58a8a5e9bc
itzyaieeshu <62168657+itzyaieeshu@users.noreply.github.com>
2021-05-21 00:14:34 +1000

    modal update

--- form.js
+++ form.js
@@ -1,3 +1,14 @@
 window.onload = (event) => {
-    document.getElementById("contact-submit").disabled = true;
+    document.getElementById("contact-submit").disabled = false;
 }
+document.addEventListener("click", (event) => {
+    var modal = document.getElementById("myModal");
+    var submitButton = document.getElementById("contact-submit");
+    var span = document.getElementsByClassName("close")[0];
+    submitButton.onclick = () => {
+        modal.style.display = "block";
+    }
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+});
