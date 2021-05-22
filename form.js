9569beecf26772eb6a94eb6d5be76051a933b0b8
itzyaieeshu <62168657+itzyaieeshu@users.noreply.github.com>
2021-05-22 22:34:18 +1000

    form update

--- form.js
+++ form.js
@@ -14,18 +14,40 @@ document.addEventListener("click", (event) => {
 });
 document.addEventListener("DOMContentLoaded", () => {
     const firstName = document.getElementById("firstname")
-    let nameValidation = () => {
+    const lastName = document.getElementById("lastname")
+    let nameValid = /^[A-Za-zÀ-ÖØ-öø-ÿ \-']+$/i
+    let firstNameValidation = () => {
         const firstNameMessage = document.getElementById("firstNameMessage")
-        let nameValid = /^[A-Za-zÀ-ÖØ-öø-ÿ \-']+$/i
         if (nameValid.test(firstName.value) === false) {
-            firstNameMessage.innerHTML = "Enter the valid name";
+            firstNameMessage.innerHTML = "Enter the valid Nom"
+            firstName.classList.remove("valid-box")
+            firstName.classList.add("error-box")
             return true
         } else if (nameValid.test(firstName.value) === true) {
             firstNameMessage.innerHTML = "";
+            firstName.classList.remove("error-box")
+            firstName.classList.add("valid-box")
+            return false
+        }
+    }
+    let lastNameValidation = () => {
+        const lastNameMessage = document.getElementById("lastNameMessage")
+        if (nameValid.test(lastName.value) === false) {
+            lastNameMessage.innerHTML = "Enter the valid Prénom";
+            lastName.classList.remove("valid-box")
+            lastName.classList.add("error-box")
+            return true
+        } else if (nameValid.test(lastName.value) === true) {
+            lastNameMessage.innerHTML = "";
+            lastName.classList.remove("error-box")
+            lastName.classList.add("valid-box")
             return false
         }
     }
     firstName.oninput = () => {
-        nameValidation()
+        firstNameValidation()
+    }
+    lastName.oninput = () => {
+        lastNameValidation()
     }
 })
