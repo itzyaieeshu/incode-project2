20f7a626429420caa136b71703c56fcd8cbba815
itzyaieeshu <62168657+itzyaieeshu@users.noreply.github.com>
2021-06-16 15:59:15 +1000

    scss files & sass webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,52 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   entry: {
-	home: [
-		'./assets/js/app.js',
-		'./assets/css/app.css'
-	]
+    home: ["./assets/js/app.js", "./assets/scss/_style.scss"],
   },
-  mode: 'production',
+  mode: "production",
   output: {
-	path: `${__dirname}/assets/dist`,
-	filename: 'js/bundle.js',
+    path: `${__dirname}/assets/dist`,
+    filename: "js/bundle.js",
   },
-plugins: [
-	new MiniCssExtractPlugin({
-	filename: 'css/style.css'
-	}),
-  ],
+  plugins: [new MiniCssExtractPlugin({ filename: "css/style.css" })],
   module: {
-	rules: [
-	  {
+    rules: [
+      {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
-	  {
+      {
         test: [/.css$|.scss$/],
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
-	  {
-		test: /\.(svg|gif|png|eot|woff|ttf)$/,
-		use: [
-		  'url-loader',
-		],
-	  },
-	  {
-		test: /\.js$/,
-		exclude: /(node_modules|bower_components)/,
-		use: {
-		  loader: 'babel-loader',
-		  options: {
-			presets: ['@babel/preset-env'],
-		  },
-		},
-	  },
-	],
+      {
+        test: /\.(svg|gif|png|eot|woff|ttf)$/,
+        use: ["url-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
   optimization: {
-	minimize: true,
-	minimizer: [
-	  new UglifyJsPlugin({
+    minimize: true,
+    minimizer: [
+      new UglifyJsPlugin({
         uglifyOptions: {
           output: null,
         },
       }),
-      new OptimizeCSSAssetsPlugin()
-	],
+      new OptimizeCSSAssetsPlugin(),
+    ],
   },
 };
